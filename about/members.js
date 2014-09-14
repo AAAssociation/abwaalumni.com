574bc88b1c368a3cfda5e67472e1efcbcb8e9d3d
mahirk <mahirk@intelfinity.com>
2014-09-14 00:12:26 -0700

    Added Council

---
+++ about/members.js
@@ -0,0 +1,10 @@
+
+    {
+        "Yash": ["Bhansali", "Bio Data Editing"],
+        "Asesha": ["Dayal", "Bio Data 2"],
+        "Mahir":["Kothary", "Mahir is a sophomore pursuing Computer Science with a minor in Entrepreneurship at UW. He has previously interned at Viacom, VMware and IBM. He loves, taking on programming as well as hardware projects - with an interest in IoT and BigData, EDM and revels in blending art and technology in digital photography."],
+        "Arnav":["Bhandari", "Helllo"],
+        "Krushank":["Shah", "One more"],
+        "Milvin":["Shroff", "Coming Soon.."],
+        "Ravi": ["Chandak", "Sing"]
+    }
