bf4244c734a4bb141d9fb797415ec6d73c33f1fd
mahirk <mahirk@intelfinity.com>
2014-12-30 15:27:02 +0530

    Web Updates

--- about/members.js
+++ about/members.js
@@ -1,10 +1,10 @@
-
+﻿
     {
-        "Yash": ["Bhansali", "Bio Data Editing"],
+        "Yash": ["Bhansali", "Yash is a junior at NYU Stern, studying Marketing and Psychology. He has a strong passion for startup businesses, especially ones that use technology in a meaningful way. He enjoys traveling and wildlife photography. When he’s not working or hanging out with friends, he’s usually going through random things on the internet with some electronic music in the background. Yash has been involved with the ABWA Alumni Association since its inception."],
         "Asesha": ["Dayal", "Bio Data 2"],
         "Mahir":["Kothary", "Mahir is a sophomore pursuing Computer Science with a minor in Entrepreneurship at UW. He has previously interned at Viacom, VMware and IBM. He loves, taking on programming as well as hardware projects - with an interest in IoT and BigData, EDM and revels in blending art and technology in digital photography."],
         "Arnav":["Bhandari", "Helllo"],
         "Krushank":["Shah", "One more"],
-        "Milvin":["Shroff", "Coming Soon.."],
-        "Ravi": ["Chandak", "Sing"]
+        "Milvin":["Shroff", "I am a freshman at Stony Brook University in Long Island, New York studying Biology on a pre-med track. I have a passion to travel around the world and airplanes fascinate me. I am on the practice squad of the ultimate frisbee team for my university and hope to get on the team one day. Sports is my kind of thing."],
+        "Ravi": ["Chandak", "Ravi is a Freshman pursuing Computer Science and Econometrics as a minor at National University of Singapore. He is interested in startups and is currently working with Courselooper. He is always open to new Startup ideas and loves implementing them.A passionate programmer and loves developing GUI's. Loves watching movies and TV shows. "]
     }
